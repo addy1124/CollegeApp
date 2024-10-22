56b89db2dbb4a63deb67dd8da5e8fb9029a9922d
addy1124 <addystephen21@gmail.com>
2024-10-22 13:45:12 +0000

    Add project files.

---
+++ Models/CollegeRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace CollegeApp.Models
+{
+    public static class CollegeRepository
+    {
+        public static List<Student> Students { get; set; } = new List<Student>() {
+            new Student() {
+                    Id = 1,
+                    StudentName = "Student 1",
+                    Email = "student1@gmail.com",
+                    Address = "Accra, Ghana"
+                },
+                new Student() {
+                    Id = 2,
+                    StudentName = "Student 2",
+                    Email = "student2@gmail.com",
+                    Address = "Kumasi, Ghana"
+                }
+        };
+    }
+}
